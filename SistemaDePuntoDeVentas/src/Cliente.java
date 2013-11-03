11b5a6c6d4e72224fbaffd082749121200857a2c
luillicastillo1988 <luillicastillo1988@gmail.com>
2013-11-03 20:02:15 +0000

    AGREGANDO LA CLASS CLIENTE

---
+++ SistemaDePuntoDeVentas/src/Cliente.java
@@ -0,0 +1,20 @@
+
+public class Cliente extends Persona {
+	
+	private String codigoCliente;
+	public Cliente()
+	{
+		super();
+		codigoCliente = null;
+	}
+
+	public void setCodigoCliente(String codigoCliente)
+	{
+		this.codigoCliente = codigoCliente;		
+	}
+	
+	public String getCodigoCliente()
+	{
+		return codigoCliente;
+	}
+	}
